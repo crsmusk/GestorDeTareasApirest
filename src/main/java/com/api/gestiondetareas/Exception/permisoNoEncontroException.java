75077ab4c486aba7e2b1024044afd23271f5cb55
cristian mosquera trullo <cristiantrullo41@gmail.com>
2024-10-05 15:10:43 -0500

    se agregaron las exceptiones para roles y permisos

---
+++ src/main/java/com/api/gestiondetareas/Exception/permisoNoEncontroException.java
@@ -0,0 +1,8 @@
+package com.api.gestiondetareas.Exception;
+
+public class permisoNoEncontroException extends RuntimeException{
+   
+    public permisoNoEncontroException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/api/gestiondetareas/Exception/rolNoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.api.gestiondetareas.Exception;
+
+public class rolNoEncontradoException extends RuntimeException {
+   
+    public rolNoEncontradoException(String message){
+      super(message);
+    }
+}
